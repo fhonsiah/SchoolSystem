8aa22d959a846260ac12cba1e642f71f5d8d9bd5
Japhari Mbaru <japhari.mbaru@tamisemi.go.tz>
2021-03-12 08:31:25 -0800

    Get and delete data from the db

--- src/main/java/org/school/student/controller/SubjectController.java
+++ src/main/java/org/school/student/controller/SubjectController.java
@@ -4,9 +4,10 @@ import org.school.student.entity.Subject;
 import org.school.student.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SubjectController {
@@ -18,5 +19,23 @@ public class SubjectController {
     public Subject createSubject(@RequestBody Subject subject){
 
         return subjectRepository.save(subject);
+    }
+    @GetMapping(value = "/subjects",produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Subject> subjectList(){
+
+        return subjectRepository.findAll();
+    }
+
+    @GetMapping(value = "/subject/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Optional<Subject> getSubjectById(@PathVariable("id") int id){
+
+        return subjectRepository.findById(id);
+
+    }
+
+    @DeleteMapping(value = "/subject/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteSubjectById(@PathVariable("id") int id){
+        subjectRepository.deleteById(id);
+
     }
 }
--- src/main/java/org/school/student/repository/SubjectRepository.java
+++ src/main/java/org/school/student/repository/SubjectRepository.java
@@ -5,6 +5,8 @@ import org.school.student.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject,Integer> {
 
